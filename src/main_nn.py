6e6124a2320556aa3517ee0a973102fc8baeb154
tom-north <tom.north@state51.com>
2023-11-20 11:50:26 +0000

    Updated to use all gameweek data

--- src/main_nn.py
+++ src/main_nn.py
@@ -6,6 +6,7 @@ from torch.utils.data import DataLoader, TensorDataset
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_squared_error
+from tqdm import tqdm
 
 # Define a more general neural network
 class NeuralNetwork(nn.Module):
@@ -38,12 +39,15 @@ def load_data(season, gameweek):
 def train_model(model, train_loader, criterion, optimizer, num_epochs):
     # Training loop
     for epoch in range(num_epochs):
-        for inputs, labels in train_loader:
+        for inputs, labels in tqdm(train_loader, desc=f'Epoch {epoch + 1}/{num_epochs}', leave=False):
             optimizer.zero_grad()
             outputs = model(inputs)
             loss = criterion(outputs, labels.view(-1, 1))
             loss.backward()
             optimizer.step()
+
+    # Save the trained model
+    torch.save(model.state_dict(), '../model/trained_model.pth')
 
 def predict(model, input_data):
     with torch.no_grad():
@@ -59,7 +63,7 @@ def main():
         'output_size': 1,
         'learning_rate': 0.001,
         'batch_size': 128,
-        'num_epochs': 200
+        'num_epochs': 100
     }
 
     # Instantiate the model
@@ -69,10 +73,11 @@ def main():
     criterion = nn.MSELoss()
     optimizer = model.configure_optimizers()
 
-    for season in range(2016, 2022):  # Update the range to cover the desired seasons
-        for gameweek in range(1, 39):  # There are 38 gameweeks in a season
+    for season in tqdm(range(2021, 2022), desc='Seasons'):
+        for gameweek in tqdm(range(1, 39), desc=f'{season}-{season+1} Gameweeks', leave=False):
             # Load training data
-            train_data = load_data(season, gameweek)
+            szn = '{}'.format(str(season)+'-'+str(season+1)[2:])
+            train_data = load_data(szn, gameweek)
 
             # Select features and target variable
             features = ['assists', 'minutes', 'goals_scored', 'ict_index', 'threat']
@@ -92,21 +97,44 @@ def main():
             # Train the model
             train_model(model, train_loader, criterion, optimizer, config['num_epochs'])
 
-    # Now, you can predict player total point scores for the 2022/23 season
-    # Load testing data for the 2022/23 season
-    test_data = load_data(2022, 1)  # You can adjust gameweek as needed
+    # Test the model against all gameweeks of the 2022/23 season
+    test_season = f'2022-23'
+    test_data = pd.concat([load_data(test_season, gameweek) for gameweek in range(1, 39)])
 
     # Select features for testing
+    features = ['assists', 'minutes', 'goals_scored', 'ict_index', 'threat']
+    target = 'total_points'
+
     X_test = test_data[features]
+    y_test = test_data[target]
+
+    # Handle missing values
+    X_test['assists'].fillna(0, inplace=True)
+    X_test['minutes'].fillna(0, inplace=True)
+    X_test['goals_scored'].fillna(0, inplace=True)
+
+    # Convert to numeric
+    X_test['assists'] = pd.to_numeric(X_test['assists'], errors='coerce')
+    X_test['minutes'] = pd.to_numeric(X_test['minutes'], errors='coerce')
+    X_test['goals_scored'] = pd.to_numeric(X_test['goals_scored'], errors='coerce')
+
+    # Verify data types
+    print(X_test.dtypes)
 
     # Convert testing data to PyTorch tensor
     X_test_tensor = torch.tensor(X_test.values, dtype=torch.float32)
+    #y_test_tensor = torch.tensor(y_test.values, dtype=torch.float32)
 
     # Make predictions on the test set
-    predictions = predict(model, X_test_tensor)
+    with torch.no_grad():
+        y_pred_tensor = model(X_test_tensor)
 
     # Convert predictions back to a numpy array
-    print(predictions)
+    y_pred_nn = y_pred_tensor.numpy()
+
+    # Evaluate the model on the test set
+    mse_test = mean_squared_error(y_test, y_pred_nn)
+    print(f'Mean Squared Error on Test Set: {mse_test}')
 
 if __name__ == "__main__":
     main()
