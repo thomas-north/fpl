7807c2dd5161b7817af47b460a97125dd85126e4
tom-north <tom.north@state51.com>
2023-11-20 15:38:19 +0000

    Including visualisation

--- src/main_nn.py
+++ src/main_nn.py
@@ -8,6 +8,8 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_squared_error
 from tqdm import tqdm
 
+SHOW_PLOTS = True
+
 # Define a more general neural network
 class NeuralNetwork(nn.Module):
     def __init__(self, input_size, num_layers, hidden_size, output_size, learning_rate, batch_size, num_epochs):
@@ -40,6 +42,9 @@ def train_model(model, train_loader, criterion, optimizer, num_epochs):
     # Training loop
     for epoch in range(num_epochs):
         for inputs, labels in tqdm(train_loader, desc=f'Epoch {epoch + 1}/{num_epochs}', leave=False):
+            if inputs.size(0) < 8:
+                continue
+
             optimizer.zero_grad()
             outputs = model(inputs)
             loss = criterion(outputs, labels.view(-1, 1))
@@ -48,6 +53,7 @@ def train_model(model, train_loader, criterion, optimizer, num_epochs):
 
     # Save the trained model
     torch.save(model.state_dict(), '../model/trained_model.pth')
+
 
 def predict(model, input_data):
     with torch.no_grad():
@@ -59,10 +65,10 @@ def main():
     config = {
         'input_size': 5,  # Adjust based on your actual input size
         'num_layers': 2,
-        'hidden_size': 128,
+        'hidden_size': 64,
         'output_size': 1,
         'learning_rate': 0.001,
-        'batch_size': 128,
+        'batch_size': 64,
         'num_epochs': 100
     }
 
@@ -73,7 +79,7 @@ def main():
     criterion = nn.MSELoss()
     optimizer = model.configure_optimizers()
 
-    for season in tqdm(range(2021, 2022), desc='Seasons'):
+    for season in tqdm(range(2016, 2022), desc='Seasons'):
         for gameweek in tqdm(range(1, 39), desc=f'{season}-{season+1} Gameweeks', leave=False):
             # Load training data
             szn = '{}'.format(str(season)+'-'+str(season+1)[2:])
@@ -85,6 +91,18 @@ def main():
 
             X = train_data[features]
             y = train_data[target]
+
+            # Handle missing or non-numeric values in X
+            X = X.apply(pd.to_numeric, errors='coerce')
+
+            # Drop rows with missing values
+            X = X.dropna()
+
+            if X.empty:
+                print(f"No data available for {season}-{season+1} Gameweek {gameweek}. Skipping...")
+                continue
+
+            y = y.loc[X.index]
 
             # Convert pandas DataFrames to PyTorch tensors
             X_train_tensor = torch.tensor(X.values, dtype=torch.float32)
@@ -100,6 +118,8 @@ def main():
     # Test the model against all gameweeks of the 2022/23 season
     test_season = f'2022-23'
     test_data = pd.concat([load_data(test_season, gameweek) for gameweek in range(1, 39)])
+
+    # ... (rest of your testing code)
 
     # Select features for testing
     features = ['assists', 'minutes', 'goals_scored', 'ict_index', 'threat']
@@ -135,6 +155,29 @@ def main():
     # Evaluate the model on the test set
     mse_test = mean_squared_error(y_test, y_pred_nn)
     print(f'Mean Squared Error on Test Set: {mse_test}')
+
+    # Some plotting routines
+    if SHOW_PLOTS == True:
+        import matplotlib.pyplot as plt
+
+        # Convert predicted values back to a numpy array
+        y_pred_nn = y_pred_tensor.numpy()
+
+        # Create a scatter plot
+        plt.scatter(y_test, y_pred_nn, alpha=0.5)
+        plt.title('Actual vs. Predicted Total Points')
+        plt.xlabel('Actual Total Points')
+        plt.ylabel('Predicted Total Points')
+        plt.show()
+
+        residuals = y_test - y_pred_nn.flatten()
+
+        plt.scatter(y_test, residuals, alpha=0.5)
+        plt.axhline(y=0, color='r', linestyle='--')
+        plt.title('Residuals Plot')
+        plt.xlabel('Actual Total Points')
+        plt.ylabel('Residuals')
+        plt.show()
 
 if __name__ == "__main__":
     main()
